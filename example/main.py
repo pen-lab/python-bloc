f02df082eed01b93b4cea5a43d550b10a8c303be
pen-dev <pelipenkovladislav@gmail.com>
2021-01-08 22:31:04 +0300

    Пример счетчика

--- example/main.py
+++ example/main.py
@@ -1,14 +1,16 @@
 import asyncio
 from abc import ABC
 
-from typing import Final
 
-from bloc.bloc import Bloc
+from bloc import (
+    BlocSupervisor,
+    BlocDelegate,
+    Transition,
+    Stream,
+    Bloc
+)
+from bloc._bloc.bloc import E, S
 
-from aioreactive.subject import AsyncMultiSubject as Stream
-
-
-l = []
 
 class Event(ABC):
     pass
@@ -22,38 +24,42 @@ class Decrement(Event):
     pass
 
 
-class CounterBloc(Bloc[Event, str]):
+class CounterBloc(Bloc[Event, int]):
 
     @property
-    def initial_state(self) -> str:
-        return 'empty'
+    def initial_state(self) -> int:
+        return 10
 
-    async def map_event_to_state(self, event: Event) -> Stream[str]:
+    async def map_event_to_state(self, event: Event) -> Stream[int]:
 
         if isinstance(event, Increment):
             await asyncio.sleep(5)
-            return 'state 1'
+            return self.state + 1
         else:
-            return 'state 2'
+            return self.state - 1
+
+    async def on_transition(self, transition: Transition[Event, int]) -> None:
+        print(f'{transition}')
+
+
+class SimpleBlocDelegate(BlocDelegate):
+
+    async def on_transition(self, transition: Transition) -> None:
+        print(f'{transition}')
 
 
 async def main(bloc) -> None:
     await bloc._bind_state_subject()
-    await bloc.dispatch(Increment())
-    await bloc.dispatch(Decrement())
-    await bloc.dispatch(Increment())
-    # await asyncio.gather(
-    #     bloc.dispatch(Increment()),
-    #     bloc.dispatch(Increment()),
-    #     bloc.dispatch(Increment()),
-    #     bloc.dispatch(Decrement()),
-    #     bloc.dispatch(Increment())
-    # )
+
+    await asyncio.gather(
+        bloc.dispatch(Increment()),
+        bloc.dispatch(Decrement()),
+        bloc.dispatch(Increment())
+    )
+
 
 if __name__ == '__main__':
-
-    counter_bloc = CounterBloc(Increment())
+    BlocSupervisor().delegate = SimpleBlocDelegate()
+    counter_bloc = CounterBloc()
 
     asyncio.run(main(counter_bloc))
-
-    print(l)
