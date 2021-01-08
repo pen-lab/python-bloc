a224821b99042e4734b579b975c081eb0f928111
pen-dev <pelipenkovladislav@gmail.com>
2021-01-08 22:28:31 +0300

    события временно не передаются

--- bloc/_bloc/bloc.py
+++ bloc/_bloc/bloc.py
@@ -10,7 +10,14 @@ from .transition import Transition
 from .bloc_supervisor import bloc_supervisor
 
 E = TypeVar('E')
+"""
+Docs
+"""
+
 S = TypeVar('S')
+"""
+Docs
+"""
 
 
 class Bloc(ABC, Generic[E, S]):
@@ -32,14 +39,15 @@ class Bloc(ABC, Generic[E, S]):
         async def _map_state_to_transition(next_state: S) -> None:
             transititon = Transition[E, S](
                 current_state=self.state,
-                event=None,
+                # TODO: transmit current event
+                # event=None,
                 next_state=next_state
             )
             if bloc_supervisor.delegate is not None:
                 await bloc_supervisor.delegate.on_transition(transititon)
 
-            await self.on_transition(transititon)
             await self._state_subject.asend(next_state)
+            await self.on_transition(transititon)
 
         async def update_state(next_state: S):
             self._state = next_state
@@ -57,24 +65,19 @@ class Bloc(ABC, Generic[E, S]):
             rx.AsyncAnonymousObserver(asend=_map_state_to_transition)
         )
 
-    async def on_transition(self, transition: Transition) -> None:
-        print(
-            f'Current: {transition.current_state}, '
-            f'Next: {transition.next_state}, '
-            f'Event: {transition.event}'
-        )
+    @abstractmethod
+    async def on_transition(self, transition: Transition[E, S]) -> None:
+        ...
 
     @property
     def state(self) -> S:
         return self._state
 
+    # TODO: add current_state
     @abstractmethod
-    async def map_event_to_state(self, current_state: S, event: E) -> Stream[S]:
+    async def map_event_to_state(self, event: E) -> Stream[S]:
         ...
 
     async def dispose(self) -> None:
         await self._event_subject.aclose()
         await self._state_subject.aclose()
-
-
-
