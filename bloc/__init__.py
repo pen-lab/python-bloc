a5e04b804726dd43af1d5a6ffb278eee73cd3eab
pen-dev <pelipenkovladislav@gmail.com>
2021-01-08 21:59:30 +0300

    импорт Stream

--- bloc/__init__.py
+++ bloc/__init__.py
@@ -1,4 +1,4 @@
 from ._bloc.transition import Transition
 from ._bloc.bloc_delegate import BlocDelegate
 from ._bloc.bloc_supervisor import BlocSupervisor
-from ._bloc.bloc import Bloc
+from ._bloc.bloc import Bloc, Stream
